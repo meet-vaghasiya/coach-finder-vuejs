55d30f2e6b9b880facf6317ca8facdf4083c1f8a
meet-vaghasiya <meetvaghasiya2012@gmail.com>
2021-05-04 22:21:55 +0530

    loading spinner added for error handling in

--- src/main.js
+++ src/main.js
@@ -8,12 +8,15 @@ import BaseBedge from '@/components/ui/BaseBedge';
 import BaseButton from '@/components/ui/BaseButton';
 import BaseCard from '@/components/ui/BaseCard';
 import BaseSpinner from '@/components/ui/BaseSpinner';
+import BaseDialog from '@/components/ui/BaseDialog';
+
 const app = createApp(App);
 //use component
 app.component('base-bedge', BaseBedge);
 app.component('base-button', BaseButton);
 app.component('base-card', BaseCard);
 app.component('base-spinner', BaseSpinner);
+app.component('base-dialog', BaseDialog);
 
 app.use(router);
 app.use(store);
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -34,7 +34,9 @@ export default {
     );
     const responseData = await response.json();
     if (!response.ok) {
-      console.log('error');
+      // console.log('error');
+      const error = new Error(responseData.message || 'Failed to fetch');
+      throw error;
     }
     const coaches = [];
     for (const key in responseData) {
